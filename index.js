51a87a13b8344a8595c6f9d640a05a5d92fcf847
gm <gleb.mihalkov.work@yandex.ru>
2018-07-17 15:03:36 +0300

    Добавление класса элемента управления

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Widget from './src/widget.js';
 import WidgetEvent from './src/widget-event.js';
+import WidgetControl from './src/widget-control.js';
 
 export {
   Widget,
-  WidgetEvent
+  WidgetEvent,
+  WidgetControl
 };
---
+++ src/widget-control.js
@@ -0,0 +1,24 @@
+import Widget from './widget-event.js';
+
+/**
+ * Элемент управления другим виджетом.
+ */
+export default class WidgetControl extends Widget {
+
+  /**
+   * Создает экземпляр класса.
+   * @param {Widget}      widget         Виджет, которым управляет данный
+   *                                     экземпляр.
+   * @param {HTMLElement} [element=null] Элемент DOM, к которому привязан
+   *                                     экземпляр.
+   */
+  constructor(widget, element = null) {
+    super(element);
+
+    /**
+     * Виджет, которым управляет данный экземпляр.
+     * @type {void}
+     */
+    this.widget = widget;
+  }
+}
